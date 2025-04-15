fb8115d728d20b667f6466a555cb9a8d2c877294
Charaka Gunasinghe <charaka.info@gmail.com>
2025-04-15 21:09:44 +0530

    feature - global error handler implemented

--- constants/app.constants.js
+++ constants/app.constants.js
@@ -5,4 +5,21 @@ module.exports = {
     STAGE: 'staging',
     PROD: 'production',
   }),
+
+  STATUS_CODE: Object.freeze({
+    OK: 200,
+    CREATED: 201,
+
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDON: 403,
+    NOT_FOUND: 404,
+    CONFLICT: 409,
+    GONE: 410,
+    UNPORCESSABLE: 422,
+
+    SERVER_ERROR: 500,
+    BAD_GATEWAY: 502,
+    TIME_OUT: 504,
+  }),
 };
