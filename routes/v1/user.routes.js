ba0dd8354059fc684bbc4ab4cd3fb41cb2b9c66b
Charaka Gunasinghe <charaka.info@gmail.com>
2025-04-16 00:32:00 +0530

    feature - user login endpoint with jwt generation implemented

--- routes/v1/user.routes.js
+++ routes/v1/user.routes.js
@@ -4,5 +4,6 @@ const userController = require('../../controllers/v1/user.controller');
 const userRouter = express.Router();
 
 userRouter.post('/', userController.signup);
+userRouter.post('/login', userController.login);
 
 module.exports = userRouter;
--- services/v1/user.service.js
+++ services/v1/user.service.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const logger = require('../../middleware/log/logger');
+const jwtService = require('../jwt.service');
 const fieldValidator = require('../../util/fieldValidator');
 const CustomError = require('../../util/customError');
 const userDao = require('../../repositories/v1/user.dao');
@@ -59,6 +60,66 @@ const userService = {
       data: {
         user: newUser,
       },
+    };
+  },
+
+  userLogin: async (data) => {
+    const { email, password } = data;
+
+    // validate user details
+    const errorArray = [];
+    errorArray.push(await fieldValidator.validate_email(email, 'email'));
+    errorArray.push(await fieldValidator.validate_string(password, 'password'));
+
+    // check request data
+    const filteredErrors = errorArray.filter((obj) => obj !== 1);
+    if (filteredErrors.length !== 0) {
+      logger(LOG_TYPE.ERROR, false, STATUS_CODE.BAD_REQUEST, filteredErrors);
+
+      return {
+        success: false,
+        status: STATUS_CODE.BAD_REQUEST,
+        data: filteredErrors,
+      };
+    }
+
+    // get user details
+    const user = await userDao.getByEmail(email);
+    if (!user) {
+      throw new CustomError(RESPONSE.USER.INVALID_CRED, STATUS_CODE.UNAUTHORIZED);
+    }
+
+    // validate password and remove it
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      throw new CustomError(RESPONSE.USER.INVALID_CRED, STATUS_CODE.UNAUTHORIZED);
+    }
+    delete user.dataValues.password;
+
+    // check if user is active
+    if (!user.isActive) {
+      throw new CustomError(RESPONSE.USER.INACTIVE, STATUS_CODE.FORBIDDON);
+    }
+
+    // generate access token and refresh token
+    const tokenUser = {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      isActive: user.isActive,
+    };
+    const accessToken = await jwtService.generateAccessToken(tokenUser);
+    const refreshToken = await jwtService.generateRefreshToken(tokenUser);
+
+    return {
+      success: true,
+      status: STATUS_CODE.OK,
+      data: {
+        user: user,
+      },
+      accessToken: accessToken,
+      refreshToken: refreshToken,
     };
   },
 };
