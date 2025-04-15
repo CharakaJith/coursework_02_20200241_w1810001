3e680a6158a231413deb0abcd0616b11a3d17b20
Charaka Gunasinghe <charaka.info@gmail.com>
2025-04-15 19:11:46 +0530

    feature - server implemented

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('hello world...');
+const express = require('express');
+const cors = require('cors');
+const swagger = require('swagger-ui-express');
+require('dotenv').config();
+
+// initialize the express app
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Access-Token'],
+  })
+);
+app.use(express.json());
+
+// TODO: initialize database
+
+// TODO: swagger doc
+
+// TODO: setup routing paths
+
+// TODO: global custom error handler
+
+// start the server
+const ENV = process.env.ENV || 'development';
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`${ENV} | ${PORT}`);
+});
